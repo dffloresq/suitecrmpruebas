1d7635f3b4fcc9b774d74235e254e0c7b7ad82cd
dfflores <dfflores@serikat.es>
2023-01-28 11:57:26 +0100

    first commit

---
+++ cache/smarty/templates_c/%%EB^EB1^EB1D6FC4%%recaptcha_disabled.tpl.php
@@ -0,0 +1,21 @@
+<?php /* Smarty version 2.6.33, created on 2023-01-27 08:56:56
+         compiled from C:%5Cxampp%5Chtdocs%5Csuitecrm%5Cinclude%5Cutils/recaptcha_disabled.tpl */ ?>
+<?php echo '
+<script>
+
+  /**
+   * Login Screen Validation
+   */
+  function validateAndSubmit() {
+      generatepwd();
+    }
+
+  /**
+   * Password reset screen validation
+   */
+  function validateCaptchaAndSubmit() {
+      document.getElementById(\'username_password\').value = document.getElementById(\'new_password\').value;
+      document.getElementById(\'ChangePasswordForm\').submit();
+    }
+</script>
+'; ?>
---
+++ custom/Extension/modules/Prospects/Ext/Vardefs/_override_sugarfield_jjwg_maps_lat_c.php
@@ -0,0 +1,5 @@
+<?php
+ // created: 2023-01-27 08:56:05
+$dictionary['Prospect']['fields']['jjwg_maps_lat_c']['inline_edit']=1;
+
+ ?>
